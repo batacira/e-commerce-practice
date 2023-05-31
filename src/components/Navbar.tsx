d3ef669617a9be421491f3b4aef7fb2560e20cc5
batacira <batacira@gmail.com>
2023-05-31 15:02:40 +0200

    Modified navbar layout, added product count circle, and restyled header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,59 +1,75 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingCart } from 'react-icons/fi';
 import logo from '../assets/images/fortnite-logo-1024x324-removebg-preview.png'
 import SearchBar from "./SearchBar";
 
 const Navbar = () => {
+    const [expanded, setExpanded] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    };
+
     return (
-        <nav className="tw-flex tw-px-4 tw-items-center tw-max-w-7xl tw-m-auto tw-text-white">
-            <div className="tw-w-48 " >
+
+
+
+        <nav className="tw-flex tw-justify-between tw-px-4 tw-items-center tw-max-w-7xl tw-m-auto tw-text-white">
+            <div className="tw-w-48 tw-flex-shrink-0" >
                 <Link to='/'>
                     <img src={logo} alt="logo" />
                 </Link>
             </div>
-            <ul className="tw-flex tw-mx-auto tw-ml-12 tw-space-x-4">
-                <li>
-                    <Link to={'/back-blings'}>
-                        Back Blings
+            {!expanded && <div className="tw-flex tw-justify-between tw-w-96">
+                <ul className='tw-flex tw-space-x-4' >
+                    <li>
+                        <Link to={'/back-blings'}>
+                            Back Blings
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/gliders'}>
+                            Gliders
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/pickaxes'}>
+                            Pickaxes
+                        </Link>
+                    </li>
+
+                </ul>
+            </div>}
+            <div className="tw-flex ">
+                <SearchBar expanded={expanded} setExpanded={setExpanded} />
+            </div>
+            <div className={`tw-flex tw-space-x-2`}>
+                <ul className="tw-flex tw-space-x-3 tw-items-center tw-mr-20">
+                    <li className="tw-flex tw-space-x-2">
+                        <Link to='/srpski'>Srpski</Link>
+                        <span> | </span>
+                        <Link to='/english'>English</Link>
+                    </li>
+                    <li className="tw-relative tw-mr-2">
+                        <Link to='/card'>
+                            <FiShoppingCart size={24} />
+                            <div className="tw-absolute tw-bottom-4 tw-left-2 tw-flex tw-items-center tw-justify-center tw-w-5 tw-h-5 tw-bg-red-500 tw-rounded-full tw-text-white tw-text-xs">
+                                2
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="tw-flex tw-space-x-1">
+                    <Link to={'/sign-up'}>
+                        SignUp
                     </Link>
-                </li>
-                <li>
-                    <Link to={'/gliders'}>
-                        Gliders
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/pickaxes'}>
-                        Pickaxes
-                    </Link>
-                </li>
-                <li>
                     <Link to={'/login'}>
                         Login
                     </Link>
-                </li>
-                <li>
-                    <Link to={'/sign-up'}>
-                        Sign Up
-                    </Link>
-                </li>
-            </ul>
-            <ul className="tw-flex tw-space-x-3 tw-items-center">
-                <div>
-                    <SearchBar />
                 </div>
-                <li>
-                    <Link to='/srpski'>Srpski</Link>
-                    <span> | </span>
-                    <Link to='/english'>English</Link>
-                </li>
-                <li>
-                    <Link to='/card'>
-                        <FiShoppingCart />
-                    </Link>
-                </li>
-            </ul>
-
+            </div>
         </nav>
     )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { SetStateAction, Dispatch } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 
-const SearchBar = () => {
-    const [expanded, setExpanded] = useState(false);
+interface SearchBarProps {
+    expanded: boolean;
+    setExpanded: Dispatch<SetStateAction<boolean>>
+}
+
+const SearchBar = ({ expanded, setExpanded }: SearchBarProps) => {
     console.log(expanded);
 
 
@@ -11,8 +15,8 @@ const SearchBar = () => {
     };
     return (
         <div className='tw-flex tw-relative tw-text-black'>
-            <input type="text" onClick={handleInputClick} className={`tw-py-2 tw-px-4 tw-duration-500 tw-ease-in-out tw-height-200px  ${expanded ? ' tw-w-[500px] tw-z-10' : 'tw-z-0 tw-w-36'}`} />
-            <AiOutlineSearch size='28px' color='gray' className='tw-absolute tw-right-2 tw-top-1 tw-flex tw-items-center tw-pl-1 tw-cursor-pointer tw-z-20' onClick={() => setExpanded(false)} />
+            <input type="text" onClick={handleInputClick} className={`tw-py-2 tw-px-4 tw-transition-transform  tw-duration-500 tw-ease-in-out  ${expanded ? '-translate-x-full tw-w-[650px] tw-z-10' : ''}`} />
+            <AiOutlineSearch size='28px' color='gray' className={`tw-absolute tw-right-2 tw-top-1 tw-flex tw-items-center tw-pl-1 tw-cursor-pointer tw-z-20`} onClick={() => setExpanded(false)} />
         </div>
     )
 }
@@ -20,5 +24,3 @@ const SearchBar = () => {
 export default SearchBar
 
 
-/* tw-transition-width tw-duration-300 tw-ease-in-out tw-w-200 tw-focus:w-full */
-/* tw-w-80 * / /* ovo radi */
